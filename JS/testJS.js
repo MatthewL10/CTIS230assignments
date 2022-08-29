6aa7e17dc94e0f1a616e273731bc727485b2fd7c
MatthewL10 <52021014+MatthewL10@users.noreply.github.com>
2022-08-29 16:04:06 -0400

    added JS test and quirie

--- JS/testJS.js
+++ JS/testJS.js
@@ -29,6 +29,8 @@ function onMouseOver() {
   ourText.innerHTML = "And we changed the text";
 }
 
+myCard = document.getElementById("card");//get the div that holds the card image
+
 ourText.addEventListener("mouseout",onMouseOut);
 function onMouseOut(){
   ourText.style.color = "black";
@@ -42,5 +44,46 @@ ourButton.addEventListener("click", onClick);
 function onClick(){
     ourText.innerHTML="I clicked the button";
     ourText.style.color = "red";
+myCard.style.display= "block";
+}
+
+// get the textfield with the id myinput
+ourInput = document.getElementById("myinput"); // this contains the new text
+ourHeading = document.getElementById("header"); // what we're going to modify
+
+// tell the textfield what event listener is connected to its events
+ourInput.addEventListener("change", changeText); // the text int he field changes
+ourInput.addEventListener("blur", changeText);
+
+//the textfield has focus when we're typing into it
+// when we click out of it, the text field loses foucs, and theat generates
+// a blur event 
+
+function changeText() {
+    // Get the text that the user entered
+    newtext = ourInput.value; // we ask our input for its value , which is the entered text
+    ourHeading.innerHTML = newtext;
+}
+myCardImage = document.getElementById("cardimage");
+myCard.addEventListener("click", changeImage);
+
+function changeImage() {
+  if (myCard.style.position == ""){
+    myCard.style.position = "absolute"; // make the position of the div absolute and 100px from the top left corner
+    myCard.style.top= "100px";
+    myCard.style.left="100px";
+    myCardImage.style.width="80px"; // make the width 
+    // not the wdith of the div because that won't after\
+    myCardImage.src="jsImages/2_of_diamonds.png";
+  }else{
+    myCard.style.position = ""; //""means use orginal value
+    myCard.style.top="";
+    myCard.style.left="";
+    myCardImage.style.width = "auto";
+    myCardImage.src="jsImages/2_of_clubs.png";
+
+  }
+
+
 
 }
