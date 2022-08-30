f118f97dea9d29b06f4c83371a3cee736bf66da9
MatthewL10 <52021014+MatthewL10@users.noreply.github.com>
2022-08-29 22:16:12 -0400

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,15 +1,15 @@
 myitem = document.getElementById("firsttest");
 //creates a variable that represents the text selected using the id
 myitem.addEventListener("click", onClick);
-myitem.addEventListener("mouseout",mouseOut);
+// myitem.addEventListener("mouseout",mouseOut);
 //creates the eventlistner and establishes its type
 function onClick() {  
     myitem.style.color = "green";  
     myitem.style.fontSize = "36px";
 }
 //establishes what the fucntion does
-function mouseOut(){
-    myitem.style.color = "";
-    myitem.style.fontSize = "";
+// function mouseOut(){
+//     myitem.style.color = "";
+//     myitem.style.fontSize = "";
 
-}
+// }
